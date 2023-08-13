c45c212781eb858c7f56618ab7d973af5774ec96
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-08-14 00:09:50 +0200

    this commit introduces a Python program to remove all characters except a specified character from a given string

---
+++ remove_all_characters_except_a_specified_character.py
@@ -0,0 +1,14 @@
+'''
+Write a Python program to remove all characters except a specified character from a given string
+'''
+sentence=input('enter a sentence or a word')
+remained=input('enter a character which will remain')
+def remove_all_characters(sentencee,character):
+    corrected=''
+    for element in sentencee:
+        if (element.upper() or element.lower)==character:
+            corrected+=element
+        else:
+            continue
+    print(corrected)
+remove_all_characters(sentence,remained)
