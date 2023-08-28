d7d48ad9d6642854e2af321d5ce054100f931915
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-08-28 23:31:50 +0200

    add key to dictionary

--- a_Python_script_to_add_a_key_to_a_dictionary.py
+++ a_Python_script_to_add_a_key_to_a_dictionary.py
@@ -1,6 +1,8 @@
-# Write a Python script to add a key to a dictionary.
+'''
+#Write a Python script to add a key to a dictionary.
 # Sample Dictionary : {0: 10, 1: 20}
 # Expected Result : {0: 10, 1: 20, 2: 30}
+'''
 
 dictionary = {0: 10, 1: 20}
 dictionary[2] = 30
