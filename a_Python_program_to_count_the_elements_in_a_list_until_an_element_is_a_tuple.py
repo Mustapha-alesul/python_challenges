a23556adab859bac657265415aab4477d448f9d8
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-08-30 15:01:16 +0200

    # Write a Python program to sort a tuple by its float element.

---
+++ a_Python_program_to_count_the_elements_in_a_list_until_an_element_is_a_tuple.py
@@ -0,0 +1,12 @@
+'''
+# Write a Python program to count the elements in a list until an element is a tuple.
+'''
+
+mixed_list = [1, "apple", (2, 3), "banana", (4, 5), 6.5, {"name": "Alice", "age": 25}]
+count = 0
+for item in mixed_list:
+    if isinstance(item,tuple):
+        break
+    else:
+        count += 1
+print(count)
